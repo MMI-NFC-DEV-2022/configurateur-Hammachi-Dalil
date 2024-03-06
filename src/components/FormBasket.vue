769f15fb04a7ad5b04b72d1840f6ff619ca794e8
Hammachi-Dalil <116789242+Hammachi-Dalil@users.noreply.github.com>
2024-03-06 13:32:33 +0100

    Label personnalisé par "slot"

--- src/components/FormBasket.vue
+++ src/components/FormBasket.vue
@@ -31,14 +31,78 @@ const chaussure = ref<Basket>(props.data ?? {});
 </div>
 
 <FormKit type="form" v-model="chaussure">
-<FormKit name="semelle" label="semelle" value="#000000" type="radio" :options="colors" />
-<FormKit name="empeigne" label="empeigne" value="#FFFFFF" type="radio" :options="colors" />
-<FormKit name="pointe" label="pointe" value="#FFFFFF" type="radio" :options="colors" />
-<FormKit name="oeillet" label="oeillet" value="#FFFFFF" type="radio" :options="colors" />
-<FormKit name="bande" label="bande" value="#FFFFFF" type="radio" :options="colors" />
-<FormKit name="languette" label="languette" value="#000000" type="radio" :options="colors" />
-<FormKit name="lacet" label="lacet" value="#FFFFFF" type="radio" :options="colors" />
-<FormKit name="trimestre" label="trimestre" value="#FFFFFF" type="radio" :options="colors" />
+    <FormKit name="semelle" label="semelle" value="#000000" type="radio" :options="colors" options-class="flex m-2" >
+        <template #label="context">
+            <div
+                class="h-6 w-6 rounded-full"
+                :style="{ backgroundColor: context.option.value }" 
+                />
+                <span>{{ context.option.label }}</span>
+        </template>
+    </FormKit>
+    <FormKit name="empeigne" label="empeigne" value="#FFFFFF" type="radio" :options="colors" options-class="flex m-2">
+        <template #label="context">
+            <div
+                class="h-6 w-6 rounded-full"
+                :style="{ backgroundColor: context.option.value }" 
+                />
+                <span>{{ context.option.label }}</span>
+        </template>
+    </FormKit>
+    <FormKit name="pointe" label="pointe" value="#FFFFFF" type="radio" :options="colors" options-class="flex m-2">
+        <template #label="context">
+            <div
+                class="h-6 w-6 rounded-full"
+                :style="{ backgroundColor: context.option.value }" 
+                />
+                <span>{{ context.option.label }}</span>
+        </template>
+    </FormKit>
+    <FormKit name="oeillet" label="oeillet" value="#FFFFFF" type="radio" :options="colors" options-class="flex m-2">
+        <template #label="context">
+            <div
+                class="h-6 w-6 rounded-full"
+                :style="{ backgroundColor: context.option.value }" 
+                />
+                <span>{{ context.option.label }}</span>
+        </template>
+    </FormKit>
+    <FormKit name="bande" label="bande" value="#FFFFFF" type="radio" :options="colors" options-class="flex m-2">
+        <template #label="context">
+            <div
+                class="h-6 w-6 rounded-full"
+                :style="{ backgroundColor: context.option.value }" 
+                />
+                <span>{{ context.option.label }}</span>
+        </template>
+    </FormKit>
+    <FormKit name="languette" label="languette" value="#000000" type="radio" :options="colors" options-class="flex m-2">
+        <template #label="context">
+            <div
+                class="h-6 w-6 rounded-full"
+                :style="{ backgroundColor: context.option.value }" 
+                />
+                <span>{{ context.option.label }}</span>
+        </template>
+    </FormKit>
+    <FormKit name="lacet" label="lacet" value="#FFFFFF" type="radio" :options="colors" options-class="flex m-2">
+        <template #label="context">
+            <div
+                class="h-6 w-6 rounded-full"
+                :style="{ backgroundColor: context.option.value }" 
+                />
+                <span>{{ context.option.label }}</span>
+        </template>
+    </FormKit>
+    <FormKit name="trimestre" label="trimestre" value="#FFFFFF" type="radio" :options="colors" options-class="flex m-2">
+        <template #label="context">
+            <div
+                class="h-6 w-6 rounded-full"
+                :style="{ backgroundColor: context.option.value }" 
+                />
+                <span>{{ context.option.label }}</span>
+        </template>
+    </FormKit>
 
 </FormKit>
 </div>
